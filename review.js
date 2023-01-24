8c5249201a39265917b27929aa8d92039bb0ad3f
Abdulloh2211 <abdulloh5670089@gmail.com>
2023-01-24 12:24:46 +0500

    git hub

---
+++ review.js
@@ -0,0 +1,13 @@
+// reposigittory = project
+
+// connection
+//   1. git init
+//   2. git remote add origin https://github.com/Abdulloh2211/task1.git
+
+
+// branch= bu qo'shimcha uning ustida ishlash stoli
+//push= bu  dokumentdi jo'natish 
+// 1. git add. - bu  bizga papkani ochib beradi . bu polniy papkani tochkasiz bersak bu bironta ichki kichkina papkani ochib beradi
+// 2. git commit -m 'test' bu bizga papkani oldida kichik information beradi
+// 3. git push origin master  
+
