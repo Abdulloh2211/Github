381ec0f603e802f45f01cd29925b0286f6a59a33
Abdulloh2211 <abdulloh5670089@gmail.com>
2023-01-24 20:01:30 +0500

    test

--- review.js
+++ review.js
@@ -13,4 +13,3 @@
 // 1. git add. - bu  bizga papkani ochib beradi . bu polniy papkani tochkasiz bersak bu bironta ichki kichkina papkani ochib beradi
 // 2. git commit -m 'test' bu bizga papkani oldida kichik information beradi
 // 3. git push origin master  
-
