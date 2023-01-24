e594fd5d285eda90a809cec9b27d8f289eabe68b
Abdulloh2211 <abdulloh5670089@gmail.com>
2023-01-24 21:53:14 +0500

    yangi

--- review.js
+++ review.js
@@ -8,8 +8,17 @@
 // git config --global user.email "you@example.com"
 // git config --global user.name "Your Name"
 
-// branch= bu qo'shimcha uning ustida ishlash stoli
+
+// branch= bu qo'shimcha uning ustida ishlash stoligit
+// git branch  -bu branch yaratish
+// git checkout branchname -bu masterdan branchga o'tish
+// git branch -m branchname - bu tepadagi 2narsani qisqartmasi
+
+// pull=bu dokimentdi tortib olish
+// 1.git pull origin master - bu masterga tortib olishga ishlaydi
+
 //push= bu  dokumentdi jo'natish 
 // 1. git add. - bu  bizga papkani ochib beradi . bu polniy papkani tochkasiz bersak bu bironta ichki kichkina papkani ochib beradi
 // 2. git commit -m 'test' bu bizga papkani oldida kichik information beradi
-// 3. git push origin master  
+// 3. git push origin master  -bu masterga push qilgani 
+
